fe538c1f240cceccc2d77abb8c271ab2e34ebdbb
sasha <alextemchur7@gmail.com>
2022-09-08 21:47:26 +0300

    Виправлення багів у 2 функціях в playlist.py

--- playlist.py
+++ playlist.py
@@ -1,41 +1,24 @@
 import sqlite3
 
-db = sqlite3.connect('db.bin')
+def adding_a_playlist(add_name_playlist):
+    connection = sqlite3.connect('db.bin')
+    cursor = connection.cursor()    
+    if add_name_playlist == '':
+        return "Error"
+    else: 
+        cursor.execute('INSERT INTO playlists (playlist) VALUES ("{}")'.format(add_name_playlist))
+        return "Add a playlist"
+    connection.commit()
+    connection.close()
 
-c = db.cursor()
+          
+def delete_a_playlist(delete_id_playlist):
+    connection = sqlite3.connect('db.bin')
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM playlists WHERE id = "{}"'.format(delete_id_playlist))
+    return "Delete playlist"
+    connection.commit()
+    connection.close()
 
-def adding_a_playlist():
-    add_name_playlist = str(input("Which playlist do you want to add?:"))
-    c.execute('SELECT * FROM playlists')
-    extracts_the_table_from_the_database = c.fetchall()
-    if add_name_playlist != '':
-        for i in range(len(extracts_the_table_from_the_database)):
-            if add_name_playlist in extracts_the_table_from_the_database[i]:
-                return "\nSuch a playlist exists\n"
-        else: 
-            c.execute('INSERT INTO playlists (playlist) VALUES ("{}")'.format(add_name_playlist))
-    else:
-        print("Error") 
-
-
-
-        
-                 
-def delete_a_playlist():
-    delete_name_playlist = str(input("Which playlist do you want to delete?:"))
-    c.execute('SELECT * FROM playlists')
-    extracts_the_table_from_the_database = c.fetchall()
-    if delete_name_playlist != '':
-        for i in range(len(extracts_the_table_from_the_database)):
-            if delete_name_playlist in extracts_the_table_from_the_database[i]:
-                delete_playlist = c.execute('DELETE FROM playlists WHERE playlist = "{}"'.format(delete_name_playlist))
-                return delete_playlist
-        else: 
-            return "\nSuch a playlist exists\n"
-    else:
-        print("Error")
-print(adding_a_playlist())
-print(delete_a_playlist())
-
-db.commit()
-db.close()
+print(adding_a_playlist(str(input("Add Playlist:"))))
+print(delete_a_playlist(str(input("Delete Playlist:"))))
