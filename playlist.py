e9be19969767d7d9db557eb1a1823021761fc5f2
sasha <alextemchur7@gmail.com>
2022-09-10 00:03:06 +0300

    Повністю дороблені 2 функції у playlist.py

--- playlist.py
+++ playlist.py
@@ -1,24 +1,25 @@
 import sqlite3
 
 def adding_a_playlist(add_name_playlist):
-    connection = sqlite3.connect('db.bin')
-    cursor = connection.cursor()    
     if add_name_playlist == '':
         return "Error"
-    else: 
+    else:     
+        connection = sqlite3.connect('db.bin')
+        cursor = connection.cursor()    
         cursor.execute('INSERT INTO playlists (playlist) VALUES ("{}")'.format(add_name_playlist))
+        connection.commit()
+        connection.close()
         return "Add a playlist"
-    connection.commit()
-    connection.close()
-
-          
+         
 def delete_a_playlist(delete_id_playlist):
+    music_list = []
     connection = sqlite3.connect('db.bin')
     cursor = connection.cursor()
     cursor.execute('DELETE FROM playlists WHERE id = "{}"'.format(delete_id_playlist))
-    return "Delete playlist"
+    cursor.execute('SELECT * FROM connect WHERE id_playlist = "{}"'.format(1))
+    playlist_id = cursor.fetchall()
+    for i in range(len(playlist_id)):
+        cursor.execute('DELETE FROM musics WHERE id = "{}"'.format(playlist_id[i][1]))
+    cursor.execute('DELETE FROM connect WHERE id_playlist = "{}"'.format(1))
     connection.commit()
-    connection.close()
-
-print(adding_a_playlist(str(input("Add Playlist:"))))
-print(delete_a_playlist(str(input("Delete Playlist:"))))
+    connection.close() 
