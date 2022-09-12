841742329ccf5ce4553b13d0edbb744d69f4014b
denys <palianytsia@denys>
2022-09-13 00:02:37 +0300

    add music

--- add-remove-music.py
+++ add-remove-music.py
@@ -1,1 +1,15 @@
+import sqlite3
 
+import sqlite3
+
+def add_music():
+
+    path_to_music = input()
+    connection = sqlite3.connect('db.bin')
+    cursor = connection.cursor()    
+    cursor.execute('INSERT INTO musics (music) VALUES ("{}")'.format(path_to_music))
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+add_music()
