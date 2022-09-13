f1840b0bc8140d8c5a652c39242713611e306abe
Sviatoslav Herasymenko <lalkabagi@gmail.com>
2022-09-13 19:25:30 +0300

    play_mucic

--- start_music.py
+++ start_music.py
@@ -1,2 +1,15 @@
+import sqlite3
+import os.path
 from playsound import playsound
-playsound('/home/Bagi/Стільниця/slava-marlow-aarne-biznes-vumen.mp3')
+
+def play_music():
+    id = input()
+    connection = sqlite3 .connect('db.bin')
+    cursor = connection.cursor()
+    id_music = cursor.execute('SELECT music FROM musics WHERE id = {}'.format(id))
+    path_music = id_music.fetchone()
+    
+    playsound("{}".format(path_music[0]))
+
+
+play_music()
