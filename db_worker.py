9ce9d18ce81365798785658a57d42117773991e6
Sviatoslav Herasymenko <lalkabagi@gmail.com>
2022-09-20 13:21:16 +0300

    update

--- db_worker.py
+++ db_worker.py
@@ -11,11 +11,3 @@ def create_datebase():
     connection.commit()
 
     connection.close()
-
-def wave_music(id):
-
-    connection = sqlite3.connect('db.bin')
-    cursor = connection.cursor()
-    id_music = cursor.execute('SELECT music FROM musics WHERE id = {}'.format(id))
-    path_music = id_music.fetchone()
-    return (path_music[0])
--- start_music.py
+++ start_music.py
@@ -4,21 +4,17 @@ import sqlite3
 import os.path
 from db_worker import wave_music
 
-id = input()
-
 def pause():
-        v = input()
-        if v == "q":
                 mixer.music.pause()
 
 def play(id):
         
         mixer.init()
-        mixer.music.load("{}".format(wave_music(id)))
+        mixer.music.load("{}".format(id))
         mixer.music.play()
         while True:
-                pause()
+                pass
 
 
 
-play(id)
+
