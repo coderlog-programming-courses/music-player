6c43ab029d42a728bc26c0b30642ad00abd8597f
sasha <alextemchur7@gmail.com>
2022-10-11 14:47:19 +0300

    Не дороблені функції

--- cover_art_music_author.py
+++ cover_art_music_author.py
@@ -1,6 +1,5 @@
-from loguru import logger
 import os
-
+from db_worker import does_file_exist_db, delete_music_db
 
 
 def author_photo_name_music(way):
@@ -10,8 +9,21 @@ def author_photo_name_music(way):
     dash = music.find('-')
     music_author = music[:dash]
     music_name = music[dash + 2:]
-    music_name = music_name.replace('.mp3', '').replace('-', '') 
+    music_name = music_name.replace('.mp3', '')
     return (music_author, music_name)
     
     
-print(author_photo_name_music('/home/sasha/Загрузки/Егор Крид feat. Филипп Киркоров - Цвет Настроения Чёрный.mp3'))
+#print(author_photo_name_music('/home/sasha/Музика/Егор Крид feat. Филипп Киркоров - Цвет Настроения Чёрный.mp3'))
+def does_file_exist():
+    index = 0
+    output_musics = does_file_exist_db()
+    for i in range(len(output_musics)):
+        way = os.path.relpath(output_musics[i][index], start=None)    
+        s = os.path.exists(way) 
+        if s != True:
+            
+            delete_music_db(s)
+    index += 1
+
+print(does_file_exist())
+#'/home/sasha/Загрузки/Егор Крид feat. Филипп Киркоров - Цвет Настроения Чёрный.mp3'
