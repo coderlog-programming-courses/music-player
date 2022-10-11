6b7ac3517aada0662b3e9abe2fdf19380c35d1ce
sasha <alextemchur7@gmail.com>
2022-10-11 19:32:57 +0300

    Доробив функцію яка виводить шлях якого немає

--- cover_art_music_author.py
+++ cover_art_music_author.py
@@ -1,5 +1,5 @@
 import os
-from db_worker import does_file_exist_db, delete_music_db
+from db_worker import does_file_exist_db
 
 
 def author_photo_name_music(way):
@@ -11,22 +11,22 @@ def author_photo_name_music(way):
     music_name = music[dash + 2:]
     music_name = music_name.replace('.mp3', '')
     return (music_author, music_name)
-<<<<<<< HEAD
-    
-    
+
+
 #print(author_photo_name_music('/home/sasha/Музика/Егор Крид feat. Филипп Киркоров - Цвет Настроения Чёрный.mp3'))
 def does_file_exist():
+    # Робить функцію яка виводить список музик якіх не існує
+    absent_music = []
+    output_musics = does_file_exist_db()    
     index = 0
-    output_musics = does_file_exist_db()
     for i in range(len(output_musics)):
         way = os.path.relpath(output_musics[i][index], start=None)    
         s = os.path.exists(way) 
         if s != True:
-            
-            delete_music_db(s)
+            absent_music.append(output_musics[i][index])
     index += 1
+    return absent_music
 
-print(does_file_exist())
-#'/home/sasha/Загрузки/Егор Крид feat. Филипп Киркоров - Цвет Настроения Чёрный.mp3'
-=======
->>>>>>> origin
+
+
+
