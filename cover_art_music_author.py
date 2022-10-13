b8ecb74a1ab10db52d6e2465507685324090421e
sasha <alextemchur7@gmail.com>
2022-10-13 21:36:44 +0300

    Практично все дороблено

--- cover_art_music_author.py
+++ cover_art_music_author.py
@@ -1,19 +1,21 @@
 import os
 from db_worker import does_file_exist_db
+from mutagen.mp3 import EasyMP3
 
 
 def author_photo_name_music(way):
     # Робить функцію яка виводить обложку музики, ім'я автора та ім'я музики
+    audio_list = []
+    audio = EasyMP3(way)
+    audio = [audio.tags]
+    for i in range(len(audio)):
+        name_music = audio[i]['title'][i]
+        artist_music = audio[i]['artist'][i]
+        audio_list.append(name_music)
+        audio_list.append(artist_music)
+    return audio_list
 
-    music = os.path.basename(way)
-    dash = music.find('-')
-    music_author = music[:dash]
-    music_name = music[dash + 2:]
-    music_name = music_name.replace('.mp3', '')
-    return (music_author, music_name)
 
-
-#print(author_photo_name_music('/home/sasha/Музика/Егор Крид feat. Филипп Киркоров - Цвет Настроения Чёрный.mp3'))
 def does_file_exist():
     # Робить функцію яка виводить список музик якіх не існує
     absent_music = []
@@ -21,12 +23,7 @@ def does_file_exist():
     index = 0
     for i in range(len(output_musics)):
         way = os.path.relpath(output_musics[i][index], start=None)    
-        s = os.path.exists(way) 
-        if s != True:
+        if os.path.exists(way) != True:
             absent_music.append(output_musics[i][index])
     index += 1
     return absent_music
-
-
-
-
