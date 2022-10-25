ce3b1d891d723bfb1d7b5c5f8dc555bb8af15ec3
sasha <alextemchur7@gmail.com>
2022-10-25 21:29:12 +0300

    Вернув прошлу функцію

--- cover_art_music_author.py
+++ cover_art_music_author.py
@@ -5,15 +5,12 @@ from mutagen.mp3 import EasyMP3
 
 def author_photo_name_music(way):
     # Робить функцію яка виводить обложку музики, ім'я автора та ім'я музики
-    audio_list = []
-    audio = EasyMP3(way)
-    audio = [audio.tags]
-    for i in range(len(audio)):
-        name_music = audio[i]['title'][i]
-        artist_music = audio[i]['artist'][i]
-        audio_list.append(name_music)
-        audio_list.append(artist_music)
-    return audio_list
+    music = os.path.basename(way)
+    dash = music.find('-')
+    music_author = music[:dash]
+    music_name = music[dash + 2:]
+    music_name = music_name.replace('.mp3', '')
+    return (music_author, music_name)
 
 
 def does_file_exist():
